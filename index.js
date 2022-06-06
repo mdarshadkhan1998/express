2f0766deb048aec6101b520c7d1e094f7d78760a
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-06-06 21:03:07 +0530

    deployed

--- index.js
+++ index.js
@@ -20,33 +20,53 @@ app.post("/user/create", (req, res) => {
   });
 
 
-app.post("/user/login", (req,res,next) => {
+  app.post("/user/login", (req, res, next) => {
     if ((req.body.role) === "voter"){
-        if (!req.body.username || !req.body.password)
-        {
+        if (!req.body.username || !req.body.password){
             res.status(400).send("please provide username and password")
         }
-        else
-        {
-            fs.readFile("./db.json",{encoding: "utf-8"},(err,data) => {
-
-                let myi = JSON.parse(data);
-                for (var i=0; i<myi.user.length; i++){
-                    // console.log(myi.user[i])
-                    if (myi.user[i].username === req.body.username && myi.user[i].password === req.body.password){
-                        let token = uuid();
-                        // console.log(token)
-                        fs.write("./db.json",myi,{ encoding: "utf-8" },(err,data) => {
-                            console.log(data)
-                            // res.status(201).send(token);
-                        })
-                    }
-                }
-            })
-        }
     }
- })
+    
+    fs.readFile("./db.json", { encoding: "utf-8" }, (err, data) => {
+      let store = JSON.parse(data);
+      store.user = store.user.map((e) => {
+        if (e.username == req.body.username && e.password == req.body.password){return {...e,token: uuid()}}
+        return e;
+      });
+        fs.writeFile("./db.json", JSON.stringify(store),  { encoding: "utf-8" }, () => {res.status(200).send("login Successful")}
+      )
+    })
 
+    fs.readFile("./db.json", { encoding: "utf-8" }, (err, data) => {
+        let store = JSON.parse(data);
+        var count=1;
+        store.user = store.user.map((e) => {
+            if (e.username == req.body.username){
+                count++
+            }
+            
+          });
+          if(count==store.user.length){
+              fs.writeFile("./db.json", JSON.stringify(store),  { encoding: "utf-8" }, () => {res.status(401).send("Invalid Credentials")})
+          }
+        console.log(count)
+      })
+  });
+  
+  
+  app.post("/user/logout", (req, res, next) => {
+    fs.readFile("./db.json", { encoding: "utf-8"}, (err, data) => {
+      let store = JSON.parse(data);
+      store.user = store.user.map((e) => { if (e.token && e.username == req.body.username && e.password == req.body.password) { delete e.token }
+        // return e;
+      });
+      fs.writeFile("./db.json",JSON.stringify(store), { encoding: "utf-8" }, () => {res.status(201).send("user logged out successfully")}
+      );
+    });
+  });
+
+
+  
 const PORT = process.env.PORT || 8080
 app.listen(PORT,()=>{
     console.log("Running on http://localhost8080")
