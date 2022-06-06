05de817f2ed0a4ab33283dad7826b1d73791fb24
mdarshadkhan1998 <mdarshadkhan1998@gmail.com>
2022-06-06 17:09:49 +0530

    done

--- index.js
+++ index.js
@@ -21,30 +21,30 @@ app.post("/user/create", (req, res) => {
 
 
 app.post("/user/login", (req,res,next) => {
-    // // if ((req.body.role) === "voter"){
-    // //     if (!req.body.username || !req.body.password)
-    // //     {
-    // //         res.status(400).send("please provide username and password")
-    // //     }
-    // //     else
-    // //     {
-    //         fs.readFile("./db.json",{encoding: "utf-8"},(err,data) => {
+    if ((req.body.role) === "voter"){
+        if (!req.body.username || !req.body.password)
+        {
+            res.status(400).send("please provide username and password")
+        }
+        else
+        {
+            fs.readFile("./db.json",{encoding: "utf-8"},(err,data) => {
 
-    //             let myi = JSON.parse(data);
-    //             for (var i=0; i<myi.user.length; i++){
-    //                 // console.log(myi.user[i])
-    //                 if (myi.user[i].username === req.body.username && myi.user[i].password === req.body.password){
-    //                     let token = uuid();
-    //                     // console.log(token)
-    //                     fs.write("./db.json",myi,{ encoding: "utf-8" },(err,data) => {
-    //                         console.log(data)
-    //                         // res.status(201).send(token);
-    //                     })
-    //                 }
-    //             }
-    //         })
-    // //     }
-    // // }
+                let myi = JSON.parse(data);
+                for (var i=0; i<myi.user.length; i++){
+                    // console.log(myi.user[i])
+                    if (myi.user[i].username === req.body.username && myi.user[i].password === req.body.password){
+                        let token = uuid();
+                        // console.log(token)
+                        fs.write("./db.json",myi,{ encoding: "utf-8" },(err,data) => {
+                            console.log(data)
+                            // res.status(201).send(token);
+                        })
+                    }
+                }
+            })
+        }
+    }
  })
 
 app.listen(8080,()=>{
